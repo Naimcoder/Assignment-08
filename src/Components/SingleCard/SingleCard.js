84eca42bca2f37047a2de33db535cffc2d472b0c
Naimcoder <isteiaknaim12@gmail.com>
2022-09-28 23:49:59 +0600

    added card items

--- src/Components/SingleCard/SingleCard.js
+++ src/Components/SingleCard/SingleCard.js
@@ -8,7 +8,13 @@ console.log(props)
           <div className='card'>
                <img src={image} alt="" />
                <div className="card-info">
-
+                    <h2>{title}</h2>
+                    <p>Amet minim mollit non deserunt 
+            ullamco est sit aliqua dolor do 
+                   amet sint</p>
+                    <h4>For Age :   {age}</h4>
+                    <h4>Time required :   {time}<span><small>s</small></span></h4>
+                    <button className='Card-btn'>Add To Card</button>
                </div>
           </div>
      );
